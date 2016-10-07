0033266e806b83743981133e6ddddd7c12fa4fa0
Tufail Qurashe <tufail.javeed@gmail.com>
2016-10-07 15:38:47 +0530

    Add PUT /todos/:id

--- server.js
+++ server.js
@@ -31,12 +31,10 @@ app.get('/todos/:id',function (req,res){
 	//res.send('Asking for todo with id: ' + req.params.id);
 });
 
-//post method
-//get data from the user
+//POST Add
 app.post('/todos',function (req,res){
-	var body=req.body;
 	//picks only specified field
-	body=_.pick(body,'description','completed');
+	var body = _.pick(req.body,'description','completed');
 
 	if (!_.isBoolean(body.completed) || !_.isString(body.description) ||  body.description.trim().length === 0) {
 		return res.status(400).send()
@@ -50,7 +48,6 @@ app.post('/todos',function (req,res){
 });
 
 //DELETE /:id
-
 app.delete('/todos/:id',function (req,res){
 	var matchedTodo = _.findWhere(todos,{id: parseInt(req.params.id,10)});
 	if(!matchedTodo){
@@ -62,6 +59,33 @@ app.delete('/todos/:id',function (req,res){
 	}
 });
 
+//PUT /:id
+app.put('/todos/:id',function (req,res){
+	var body = _.pick(req.body,'description','completed');
+	var matchedTodo = _.findWhere(todos,{id: parseInt(req.params.id,10)});
+	var validAttributes={};
+
+	if(!matchedTodo){
+		return res.status(404).send();
+	}
+
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')) {
+		return res.status(400).send();
+	} 
+
+	if (body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
+		validAttributes.description = body.description;
+	} else if (body.hasOwnProperty('description')) {
+		return res.status(400).send();
+	}
+
+	//objects in js are passed by reference
+	_.extend(matchedTodo,validAttributes);
+	res.json(matchedTodo);
+
+});
 app.listen(PORT, function(){
 	console.log('Express listening on port '+ PORT + '!');
 });
